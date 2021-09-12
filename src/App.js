29961707d21ed94daa1c73462f7ea5c6bf96b91a
blackaosdragon <ka_ivj@live.com>
2021-09-12 03:08:00 -0500

    Cambios finales para hacer el fetch de los servicios de servidor ya actualizado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Contador from './components/contador.js';
 import './App.css';
-
+const url = "http://localhost:5000/"
 
 class App extends Component {
   state = {
@@ -9,20 +9,56 @@ class App extends Component {
     division: 0,
     module: 0
   }
+  senData = (refreshValue) => {
+    fetch(`${url}refresh`,{
+      method: 'POST',
+      body: JSON.stringify(refreshValue),
+      headers:{
+        'Content-Type': 'application/json' 
+      },            
+    }).then( response => {
+      return response.json();
+    }).then (data => {
+      console.log(data)
+    }).catch( err => {
+      console.log(err);
+    })
+  }
+  componentDidMount(){
+    console.log("Realizando el fetch")
+    fetch(url).then(response =>{
+      return response.json();
+    }).then( data => {
+      const { value } = data
+      this.setState({
+        value: value
+      })
+      console.log(data);
+    })
+    .catch( err => {      
+      console.log (err);
 
+    })
+  }
   add = () => {
     this.setState({
       value: this.state.value + 1,
       division: 0
     })
-        
+    let datos = {
+      value: this.state.value + 1
+    }
+    this.senData(datos)    
   }
   substract = () => {
     this.setState({
       value: this.state.value - 2,
       division: 0
     })
-    
+    let datos = {
+      value: this.state.value - 2
+    }
+    this.senData(datos) 
     
   }
   divide = () => {
@@ -32,14 +68,20 @@ class App extends Component {
       module: this.state.value % 3,
       division: 1
     })
-    
+    let datos = {
+      value: parseFloat(this.state.value) / 3,
+    }
+    this.senData(datos) 
   }
   reset = () => {
     this.setState({
       value: 0,
       division: 0   
     })
-    
+    let datos = {
+      value: 0
+    }
+    this.senData(datos)
   }
   render(){
     return (
