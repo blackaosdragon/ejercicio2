7958fd5acb23a10d8671a64f876d31d5f4bce598
blackaosdragon <ka_ivj@live.com>
2021-09-12 03:06:10 -0500

    regresando al estado normal y preparado para hacer los cambios para hacer los fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Contador from './components/contador.js';
-
 import './App.css';
+
 
 class App extends Component {
   state = {
@@ -9,17 +9,21 @@ class App extends Component {
     division: 0,
     module: 0
   }
+
   add = () => {
     this.setState({
       value: this.state.value + 1,
       division: 0
     })
+        
   }
   substract = () => {
     this.setState({
       value: this.state.value - 2,
       division: 0
     })
+    
+    
   }
   divide = () => {
     console.log("modulo",this.state.value%3)
@@ -28,12 +32,14 @@ class App extends Component {
       module: this.state.value % 3,
       division: 1
     })
+    
   }
   reset = () => {
     this.setState({
       value: 0,
       division: 0   
     })
+    
   }
   render(){
     return (
