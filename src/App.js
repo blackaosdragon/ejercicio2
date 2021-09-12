e5fb39c6bfa3f2b63a02c0de9c667b8b4e2c24b0
blackaosdragon <ka_ivj@live.com>
2021-09-11 23:16:50 -0500

    Valores listos para el primer ejercicio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    value: 0
+  }
+  add = () => {
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
+  substract = () => {
+    this.setState({
+      value: this.state.value - 2
+    })
+  }
+  divide = () => {
+    this.setState({
+      value: this.state.value / 3
+    })
+  }
+  reset = () => {
+    this.setState({
+      value: 0
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Ejercicio 2</h1>
+        <button onClick={this.add} >Add</button>
+        <button onClick={this.substract} >Substract</button>
+        <button onClick={this.divide} >Divide</button>
+        <button onClick={this.reset} >Reset</button>
+        <h1>{this.state.value}</h1>
+
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
